0b0c0fea1adbf64bc07deff741eb64ec861390db
rherlitz <rherlitz@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-02-04 15:46:29 +0000

    Added more shapes to the shape example.
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@168 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-examples/src/main/java/com/ardor3d/example/basic/ShapesExample.java
+++ ardor3d-examples/src/main/java/com/ardor3d/example/basic/ShapesExample.java
@@ -11,7 +11,6 @@
 package com.ardor3d.example.basic;
 
 import java.nio.FloatBuffer;
-import java.util.logging.Logger;
 
 import com.ardor3d.example.ExampleBase;
 import com.ardor3d.framework.FrameWork;
@@ -28,26 +27,35 @@ import com.ardor3d.scenegraph.Line;
 import com.ardor3d.scenegraph.Mesh;
 import com.ardor3d.scenegraph.Spatial;
 import com.ardor3d.scenegraph.Spatial.LightCombineMode;
-import com.ardor3d.scenegraph.event.DirtyEventListener;
-import com.ardor3d.scenegraph.event.DirtyType;
-import com.ardor3d.scenegraph.event.SceneGraphManager;
 import com.ardor3d.scenegraph.shape.Arrow;
 import com.ardor3d.scenegraph.shape.AxisRods;
 import com.ardor3d.scenegraph.shape.Box;
 import com.ardor3d.scenegraph.shape.Capsule;
+import com.ardor3d.scenegraph.shape.Cone;
 import com.ardor3d.scenegraph.shape.Cylinder;
-import com.ardor3d.scenegraph.shape.OrientedBox;
+import com.ardor3d.scenegraph.shape.Disk;
+import com.ardor3d.scenegraph.shape.Dodecahedron;
+import com.ardor3d.scenegraph.shape.Dome;
+import com.ardor3d.scenegraph.shape.GeoSphere;
+import com.ardor3d.scenegraph.shape.Hexagon;
+import com.ardor3d.scenegraph.shape.Icosahedron;
+import com.ardor3d.scenegraph.shape.MultiFaceBox;
+import com.ardor3d.scenegraph.shape.Octahedron;
+import com.ardor3d.scenegraph.shape.PQTorus;
 import com.ardor3d.scenegraph.shape.Pyramid;
+import com.ardor3d.scenegraph.shape.Quad;
+import com.ardor3d.scenegraph.shape.RoundedBox;
 import com.ardor3d.scenegraph.shape.Sphere;
+import com.ardor3d.scenegraph.shape.StripBox;
 import com.ardor3d.scenegraph.shape.Teapot;
 import com.ardor3d.scenegraph.shape.Torus;
+import com.ardor3d.scenegraph.shape.Tube;
+import com.ardor3d.scenegraph.shape.GeoSphere.TextureMode;
 import com.ardor3d.util.TextureManager;
 import com.ardor3d.util.geom.BufferUtils;
 import com.google.inject.Inject;
 
-public class ShapesExample extends ExampleBase implements DirtyEventListener {
-    private static final Logger logger = Logger.getLogger(ShapesExample.class.getName());
-
+public class ShapesExample extends ExampleBase {
     private int wrapCount;
     private int index;
 
@@ -62,27 +70,37 @@ public class ShapesExample extends ExampleBase implements DirtyEventListener {
 
     @Override
     protected void initExample() {
-        SceneGraphManager.getSceneGraphManager().addDirtyEventListener(this);
-        SceneGraphManager.getSceneGraphManager().listenOnSpatial(_root);
+        _canvas.setTitle("Shapes Example");
 
-        _canvas.setTitle("Shapes");
-
-        wrapCount = 3;
-        addMesh(new Box("TestBox", new Vector3(), 3, 3, 3));
-        addMesh(new Sphere("Sphere", 16, 16, 3));
+        wrapCount = 5;
+        addMesh(new Arrow("Arrow", 3, 1));
+        addMesh(new AxisRods("AxisRods", false, 3, 1));
+        addMesh(new Box("Box", new Vector3(), 3, 3, 3));
         addMesh(new Capsule("Capsule", 5, 5, 5, 2, 5));
-        addMesh(new Teapot("Teapot"));
-        addMesh(new Torus("Torus", 8, 8, 2, 4));
+        addMesh(new Cone("Cone", 8, 8, 2, 4));
         addMesh(new Cylinder("Cylinder", 8, 8, 2, 4));
+        addMesh(new Disk("Disk", 8, 8, 3));
+        addMesh(new Dodecahedron("Dodecahedron", 3));
+        addMesh(new Dome("Dome", 8, 8, 3));
+        addMesh(new GeoSphere("GeoSphere", true, 3, 3, TextureMode.Original));
+        addMesh(new Hexagon("Hexagon", 3));
+        addMesh(new Icosahedron("Icosahedron", 3));
+        addMesh(new MultiFaceBox("MultiFaceBox", new Vector3(), 3, 3, 3));
+        addMesh(new Octahedron("Octahedron", 3));
+        addMesh(new PQTorus("PQTorus", 5, 4, 1.5, .5, 128, 8));
         addMesh(new Pyramid("Pyramid", 2, 4));
-        addMesh(new Arrow("Arrow", 2, 4));
-        addMesh(new AxisRods("AxisRods", false, 2, 4));
-        addMesh(new OrientedBox("OrientedBox"));
+        addMesh(new Quad("Quad", 3, 3));
+        addMesh(new RoundedBox("RoundedBox", new Vector3(3, 3, 3)));
+        addMesh(new Sphere("Sphere", 16, 16, 3));
+        addMesh(new StripBox("StripBox", new Vector3(), 3, 3, 3));
+        addMesh(new Teapot("Teapot"));
+        addMesh(new Torus("Torus", 8, 8, 2, 2.5));
+        addMesh(new Tube("Tube", 2, 3, 4));
         addMesh(createLines());
 
         final TextureState ts = new TextureState();
-        ts.setTexture(TextureManager.load("images/ardor3d_white_256.jpg", Texture.MinificationFilter.Trilinear, Format.Guess,
-                true));
+        ts.setTexture(TextureManager.load("images/ardor3d_white_256.jpg", Texture.MinificationFilter.Trilinear,
+                Format.Guess, true));
         _root.setRenderState(ts);
 
         final BlendState bs = new BlendState();
@@ -108,17 +126,12 @@ public class ShapesExample extends ExampleBase implements DirtyEventListener {
     }
 
     private void addMesh(final Spatial spatial) {
-        spatial.setTranslation((index % wrapCount) * 8, (index / wrapCount) * 8, -20);
+        spatial.setTranslation((index % wrapCount) * 8 - wrapCount * 4, (index / wrapCount) * 8 - wrapCount * 4, -50);
         if (spatial instanceof Mesh) {
             ((Mesh) spatial).setRandomColors();
             ((Mesh) spatial).updateModelBound();
         }
         _root.attachChild(spatial);
         index++;
-    }
-
-    public boolean spatialDirty(final Spatial spatial, final DirtyType dirtyType) {
-        logger.info("Spatial dirty: " + spatial + " - " + dirtyType);
-        return false;
     }
 }
