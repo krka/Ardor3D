448f0fce9037768a70575c8a6ef17bac0e267ce7
pmahlen <pmahlen@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-02-26 19:09:59 +0000

    Updated mistaken copyright comments that indicated Shopzilla owned the code.
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@243 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-lwjgl/src/main/java/com/ardor3d/framework/lwjgl/LwjglLibraryPaths.java
+++ ardor3d-lwjgl/src/main/java/com/ardor3d/framework/lwjgl/LwjglLibraryPaths.java
@@ -1,7 +1,13 @@
 /**
- * Copyright (C) 2004 - 2009 Shopzilla, Inc. 
- * All rights reserved. Unauthorized disclosure or distribution is prohibited.
+ * Copyright (c) 2008-2009 Ardor Labs, Inc.
+ *
+ * This file is part of Ardor3D.
+ *
+ * Ardor3D is free software: you can redistribute it and/or modify it
+ * under the terms of its license which may be found in the accompanying
+ * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
  */
+
 
 package com.ardor3d.framework.lwjgl;
 
