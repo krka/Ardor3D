8b95a962e662b3728bdd1cbb2906cdabb5ec20e1
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-06-22 03:37:36 +0000

    fixed bleeding bug on Line in example that had no assigned texture coordinates
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@547 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-examples/src/main/java/com/ardor3d/example/basic/ShapesExample.java
+++ ardor3d-examples/src/main/java/com/ardor3d/example/basic/ShapesExample.java
@@ -31,14 +31,13 @@ import com.ardor3d.math.Vector2;
 import com.ardor3d.math.Vector3;
 import com.ardor3d.renderer.IndexMode;
 import com.ardor3d.renderer.state.BlendState;
-import com.ardor3d.renderer.state.MaterialState;
 import com.ardor3d.renderer.state.TextureState;
-import com.ardor3d.renderer.state.MaterialState.ColorMaterial;
 import com.ardor3d.scenegraph.Line;
 import com.ardor3d.scenegraph.Mesh;
 import com.ardor3d.scenegraph.Spatial;
 import com.ardor3d.scenegraph.hint.CullHint;
 import com.ardor3d.scenegraph.hint.LightCombineMode;
+import com.ardor3d.scenegraph.hint.TextureCombineMode;
 import com.ardor3d.scenegraph.shape.Arrow;
 import com.ardor3d.scenegraph.shape.AxisRods;
 import com.ardor3d.scenegraph.shape.Box;
@@ -122,10 +121,6 @@ public class ShapesExample extends ExampleBase {
         bs.setBlendEnabled(true);
         _root.setRenderState(bs);
 
-        final MaterialState ms = new MaterialState();
-        ms.setColorMaterial(ColorMaterial.Diffuse);
-        _root.setRenderState(ms);
-
         // Set up a reusable pick results
         _pickResults = new PrimitivePickResults();
         _pickResults.setCheckDistance(true);
@@ -189,6 +184,9 @@ public class ShapesExample extends ExampleBase {
         verts.put(5).put(5).put(0);
         verts.put(0).put(5).put(0);
         final Line line = new Line("Lines", verts, null, null, null);
+        // since we do not set texture coords, but we'll have a texture state applied at root, we need to turn off
+        // textures on this Line to prevent bleeding of texture coordinates from other shapes.
+        line.getSceneHints().setTextureCombineMode(TextureCombineMode.Off);
         line.getMeshData().setIndexMode(IndexMode.LineStrip);
         line.setLineWidth(2);
         line.getSceneHints().setLightCombineMode(LightCombineMode.Off);
