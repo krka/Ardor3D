bea2ded2cad8aaf0a0165158e9eb5d0c3787db52
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-06-22 04:28:11 +0000

    Added some support for setPosition and setGrabbed... probably needs some tweaks
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@548 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-awt/src/main/java/com/ardor3d/input/awt/AwtMouseManager.java
+++ ardor3d-awt/src/main/java/com/ardor3d/input/awt/AwtMouseManager.java
@@ -10,12 +10,18 @@
 
 package com.ardor3d.input.awt;
 
+import java.awt.AWTException;
 import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Point;
+import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.util.logging.Logger;
+
+import javax.swing.SwingUtilities;
 
 import com.ardor3d.image.util.AWTImageUtil;
 import com.ardor3d.input.GrabbedState;
@@ -25,17 +31,29 @@ import com.google.inject.Inject;
 
 /**
  * Implementation of the {@link com.ardor3d.input.MouseManager} interface for use with AWT windows. This implementation
- * does not support the optional {@link #setPosition(int, int)} and {@link #setGrabbed(com.ardor3d.input.GrabbedState)}
- * methods. The constructor takes an AWT {@link java.awt.Component} instance, for which the cursor is set. In a
- * multi-canvas application, each canvas can have its own AwtMouseManager instance, or it is possible to use a single
- * one for the AWT container that includes the canvases.
+ * supports the optional {@link #setGrabbed(com.ardor3d.input.GrabbedState)} and {@link #setPosition(int, int)} methods
+ * if an AWT robot can be created on the current system. The constructor takes an AWT {@link java.awt.Component}
+ * instance, for which the cursor is set. In a multi-canvas application, each canvas can have its own AwtMouseManager
+ * instance, or it is possible to use a single one for the AWT container that includes the canvases.
  */
 public class AwtMouseManager implements MouseManager {
+    private static final Logger logger = Logger.getLogger(AwtMouseManager.class.getName());
+
+    private static Cursor _transparentCursor;
+
     private final Component _component;
+    private Robot _robot;
 
     @Inject
     public AwtMouseManager(final Component component) {
         _component = component;
+
+        // Attempt to make
+        try {
+            _robot = new Robot();
+        } catch (final AWTException ex) {
+            logger.warning("Unable to create java.awt.Robot.  setPosition and setGrabbed will not be supported.");
+        }
     }
 
     public void setCursor(final MouseCursor cursor) {
@@ -58,18 +76,65 @@ public class AwtMouseManager implements MouseManager {
     }
 
     public void setPosition(final int x, final int y) {
-        throw new UnsupportedOperationException();
+        if (!isSetPositionSupported()) {
+            throw new UnsupportedOperationException();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Component c = _component;
+                if (c instanceof Frame && ((Frame) c).getComponentCount() > 0) {
+                    c = ((Frame) c).getComponent(0);
+                }
+                final Point p = new Point(x, c.getHeight() - y);
+                SwingUtilities.convertPointToScreen(p, c);
+                _robot.mouseMove(p.x, p.y);
+            }
+        });
+    }
+
+    private void recenterMouse() {
+        if (!isSetPositionSupported()) {
+            throw new UnsupportedOperationException();
+        }
+
+        Component c = _component;
+        if (c instanceof Frame && ((Frame) c).getComponentCount() > 0) {
+            c = ((Frame) c).getComponent(0);
+        }
+        setPosition(c.getWidth() / 2, c.getHeight() / 2);
     }
 
     public void setGrabbed(final GrabbedState grabbedState) {
-        throw new UnsupportedOperationException();
+        if (!isSetGrabbedSupported()) {
+            throw new UnsupportedOperationException();
+        }
+
+        if (grabbedState == GrabbedState.GRABBED) {
+            _component.setCursor(getTransparentCursor());
+        } else {
+            _component.setCursor(null);
+            if (isSetPositionSupported()) {
+                recenterMouse();
+            }
+        }
+    }
+
+    private static final Cursor getTransparentCursor() {
+        if (_transparentCursor == null) {
+            final BufferedImage cursorImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+            cursorImage.setRGB(0, 0, 0);
+            _transparentCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImage, new Point(0, 0),
+                    "empty cursor");
+        }
+        return _transparentCursor;
     }
 
     public boolean isSetPositionSupported() {
-        return false; // not supported by AWT
+        return _robot != null;
     }
 
     public boolean isSetGrabbedSupported() {
-        return false; // not supported by AWT
+        return _robot != null;
     }
 }
