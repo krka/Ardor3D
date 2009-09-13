58e0862238fa3f7a60b8dc10d4c26c787fb275a3
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-09-13 21:54:29 +0000

    added 1 samples option.  (with s, sorry... makes life easier though.)
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@720 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-examples/src/main/java/com/ardor3d/example/PropertiesDialog.java
+++ ardor3d-examples/src/main/java/com/ardor3d/example/PropertiesDialog.java
@@ -71,7 +71,7 @@ public final class PropertiesDialog extends JDialog {
     private final String[] windowedResolutions = { "640 x 480", "800 x 600", "1024 x 768", "1152 x 864" };
 
     // Array of possible samples
-    private final String[] samples = { "0 samples", "2 samples", "4 samples", "8 samples" };
+    private final String[] samples = { "0 samples", "1 samples", "2 samples", "4 samples", "8 samples" };
 
     // UI components
     private JCheckBox fullscreenBox = null;
