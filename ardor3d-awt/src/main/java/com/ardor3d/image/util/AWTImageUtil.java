ff411b8c11da3090c82660c57818abf0584b6459
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-09-13 21:53:11 +0000

    Added tinting
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@717 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-awt/src/main/java/com/ardor3d/image/util/AWTImageUtil.java
+++ ardor3d-awt/src/main/java/com/ardor3d/image/util/AWTImageUtil.java
@@ -10,6 +10,7 @@
 
 package com.ardor3d.image.util;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -18,10 +19,19 @@ import java.util.List;
 public abstract class AWTImageUtil {
 
     public static List<BufferedImage> convertToAWT(final com.ardor3d.image.Image input) {
+        return convertToAWT(input, Color.WHITE);
+    }
+
+    public static List<BufferedImage> convertToAWT(final com.ardor3d.image.Image input, final Color tint) {
 
         final int size = input.getData().size();
         final int width = input.getWidth(), height = input.getHeight();
         final List<BufferedImage> rVal = new ArrayList<BufferedImage>(size);
+
+        final double tRed = tint != null ? tint.getRed() / 255. : 1.0;
+        final double tGreen = tint != null ? tint.getGreen() / 255. : 1.0;
+        final double tBlue = tint != null ? tint.getBlue() / 255. : 1.0;
+        final double tAlpha = tint != null ? tint.getAlpha() / 255. : 1.0;
 
         for (int i = 0; i < size; i++) {
             BufferedImage image;
@@ -47,14 +57,14 @@ public abstract class AWTImageUtil {
                     for (int x = 0; x < width; x++) {
                         for (int y = 0; y < height; y++) {
                             index = (alpha ? 4 : 3) * ((height - y - 1) * width + x);
-                            r = ((data.get(index + 0)));
-                            g = ((data.get(index + 1)));
-                            b = ((data.get(index + 2)));
+                            r = (int) (((data.get(index + 0))) * tRed);
+                            g = (int) (((data.get(index + 1))) * tGreen);
+                            b = (int) (((data.get(index + 2))) * tBlue);
 
                             argb = ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
 
                             if (alpha) {
-                                a = ((data.get(index + 3)));
+                                a = (int) (((data.get(index + 3))) * tAlpha);
                                 argb |= (a & 0xFF) << 24;
                             }
 
