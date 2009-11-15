30a6cc79a2180ebc0eca251655e2ef1bfc61c2a5
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-11-15 18:52:02 +0000

    Added documentation
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@828 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-awt/src/main/java/com/ardor3d/image/util/AWTImageUtil.java
+++ ardor3d-awt/src/main/java/com/ardor3d/image/util/AWTImageUtil.java
@@ -13,47 +13,83 @@ package com.ardor3d.image.util;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.List;
 
+import com.ardor3d.image.Image;
+import com.google.common.collect.Lists;
+
+/**
+ * Utility methods for converting Ardor3D Images to AWT BufferedImages.
+ */
 public abstract class AWTImageUtil {
 
-    public static List<BufferedImage> convertToAWT(final com.ardor3d.image.Image input) {
+    /**
+     * Convert the given Ardor3D Image to a List of BufferedImages. It is a List because Ardor3D Images may contain
+     * multiple layers (for example, in the case of cube maps or 3D textures).
+     * 
+     * @param input
+     *            the Ardor3D Image to convert
+     * @return the BufferedImage(s) created in the conversion
+     */
+    public static List<BufferedImage> convertToAWT(final Image input) {
+        // convert, using a full white tint (i.e. no applied color change from original data.)
         return convertToAWT(input, Color.WHITE);
     }
 
-    public static List<BufferedImage> convertToAWT(final com.ardor3d.image.Image input, final Color tint) {
-
+    /**
+     * Convert the given Ardor3D Image to a List of BufferedImages. It is a List because Ardor3D Images may contain
+     * multiple layers (for example, in the case of cube maps or 3D textures). The given AWT Color is used to modulate
+     * or "tint" the returned image.
+     * 
+     * TODO: Add support for more formats.
+     * 
+     * @param input
+     *            the Ardor3D Image to convert
+     * @param tint
+     *            the Color to apply to the generated image
+     * @return the BufferedImage(s) created in the conversion
+     */
+    public static List<BufferedImage> convertToAWT(final Image input, final Color tint) {
+        // count the number of layers we will be converting.
         final int size = input.getData().size();
-        final int width = input.getWidth(), height = input.getHeight();
-        final List<BufferedImage> rVal = new ArrayList<BufferedImage>(size);
 
+        // grab our image width and height
+        final int width = input.getWidth(), height = input.getHeight();
+
+        // create our return list
+        final List<BufferedImage> rVal = Lists.newArrayList();
+
+        // Calculate our modulation or "tint" values per channel
         final double tRed = tint != null ? tint.getRed() / 255. : 1.0;
         final double tGreen = tint != null ? tint.getGreen() / 255. : 1.0;
         final double tBlue = tint != null ? tint.getBlue() / 255. : 1.0;
         final double tAlpha = tint != null ? tint.getAlpha() / 255. : 1.0;
 
+        // go through each layer
         for (int i = 0; i < size; i++) {
             BufferedImage image;
             final ByteBuffer data = input.getData(i);
             data.rewind();
             boolean alpha = false;
             switch (input.getFormat()) {
-                // TODO: Add support for more formats.
                 case RGBA8:
                     alpha = true;
+                    // Falls through on purpose.
                 case RGB8:
                     if (alpha) {
                         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
                     } else {
                         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
                     }
+
                     int index,
                     r,
                     g,
                     b,
                     a,
                     argb;
+
+                    // Go through each pixel
                     for (int x = 0; x < width; x++) {
                         for (int y = 0; y < height; y++) {
                             index = (alpha ? 4 : 3) * (y * width + x);
@@ -61,13 +97,16 @@ public abstract class AWTImageUtil {
                             g = (int) (((data.get(index + 1))) * tGreen);
                             b = (int) (((data.get(index + 2))) * tBlue);
 
+                            // convert to integer expression
                             argb = ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF);
 
+                            // add alpha, if applicable
                             if (alpha) {
                                 a = (int) (((data.get(index + 3))) * tAlpha);
                                 argb |= (a & 0xFF) << 24;
                             }
 
+                            // apply to image
                             image.setRGB(x, y, argb);
                         }
                     }
@@ -76,9 +115,11 @@ public abstract class AWTImageUtil {
                     throw new IllegalArgumentException("Unhandled image format: " + input.getFormat());
             }
 
+            // add to our list
             rVal.add(image);
         }
 
+        // return list
         return rVal;
     }
 
