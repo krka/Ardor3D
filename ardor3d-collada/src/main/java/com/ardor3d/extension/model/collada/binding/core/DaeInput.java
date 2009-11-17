067d44c45f35ac41e99632fd78b76a827a64fc73
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2009-11-17 02:14:46 +0000

    dropping old jibx loader
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@838 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-collada/src/main/java/com/ardor3d/extension/model/collada/binding/core/DaeInput.java
+++
@@ -1,7 +0,0 @@
-package com.ardor3d.extension.model.collada.binding.core;
-
-public interface DaeInput {
-    public String getSource();
-    public String getSemantic();
-    public Collada getRootNode();
-}
