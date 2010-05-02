2bbd55a6ddd454b5db116efe9b199e424b518be1
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2010-05-02 18:24:43 +0000

    added switch between mat4 and vec4 in padding code
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@1184 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-animation/src/main/java/com/ardor3d/extension/animation/skeletal/SkinnedMesh.java
+++ ardor3d-animation/src/main/java/com/ardor3d/extension/animation/skeletal/SkinnedMesh.java
@@ -486,6 +486,115 @@ public class SkinnedMesh extends Mesh implements PoseListener {
         }
     }
 
+    private void createJointBuffer() {
+        final float[] data;
+        if (isGpuUseMatrixAttribute()) {
+            data = reorderAndPad(convert(_jointIndices), getWeightsPerVert(), getGpuAttributeSize());
+        } else {
+            data = pad(convert(_jointIndices), getWeightsPerVert(), getGpuAttributeSize());
+        }
+        _jointIndicesBuf = BufferUtils.createFloatBuffer(_jointIndicesBuf, data);
+    }
+
+    private void createWeightBuffer() {
+        final float[] data;
+        if (isGpuUseMatrixAttribute()) {
+            data = reorderAndPad(_weights, getWeightsPerVert(), getGpuAttributeSize());
+        } else {
+            data = pad(_weights, getWeightsPerVert(), getGpuAttributeSize());
+        }
+        _weightsBuf = BufferUtils.createFloatBuffer(_weightsBuf, data);
+    }
+
+    /**
+     * Convert a short array to a float array
+     * 
+     * @param shorts
+     *            the short values
+     * @return our new float array
+     */
+    private float[] convert(final short... shorts) {
+        final float[] rval = new float[shorts.length];
+        for (int i = 0; i < rval.length; i++) {
+            rval[i] = shorts[i];
+        }
+        return rval;
+    }
+
+    /**
+     * Rearrange the data from data per element, to a list of matSide x matSide matrices, output by row as such:
+     * 
+     * row0element0, row0element1, row0element2...<br>
+     * row1element0, row1element1, row1element2...<br>
+     * row2element0, row2element1, row2element2...<br>
+     * 
+     * If there is not enough values in the source data to fill out a row, 0 is used.
+     * 
+     * @param src
+     *            our source data, stored as element0, element1, etc.
+     * @param srcElementSize
+     *            the number of values per element in our source data
+     * @param matSide
+     *            the size of the matrix edge... eg. 4 would mean a 4x4 matrix.
+     * @return our new data array.
+     */
+    private float[] reorderAndPad(final float[] src, final int srcElementSize, final int matSide) {
+        final int elements = src.length / srcElementSize;
+        final float[] rVal = new float[elements * matSide * matSide];
+
+        // size of each attribute (a row from each element)
+        final int length = matSide * elements;
+
+        for (int i = 0; i < elements; i++) {
+            // index into src for our element data
+            final int srcStart = i * srcElementSize;
+            // index into a destination row.
+            final int dstOffset = i * matSide;
+
+            // Go through each row of the current src element. Go through only as many rows of data as we have.
+            // (eg. if size is 6 and matSide is 4, we only need to go through j=0 and j=1)
+            for (int j = 0; j <= (srcElementSize - 1) / matSide; j++) {
+                // How much to copy. Generally matSide, except for last bit of data.
+                final int copySize = Math.min(srcElementSize - j * matSide, matSide);
+                // Copy the data from src to rVal
+                System.arraycopy(src, srcStart + j * matSide, rVal, j * length + dstOffset, copySize);
+            }
+        }
+
+        return rVal;
+    }
+
+    /**
+     * Expand out our src data so that each attribute is a certain size, padding with 0's as needed. If src is already
+     * correct size, we just return that without creating a new data array.
+     * 
+     * @param src
+     *            our source data, stored as element0, element1, etc.
+     * @param srcElementSize
+     *            the number of values per element in our source data
+     * @param attribSize
+     *            the desired size of each element in the return array.
+     * @return the padded array.
+     */
+    private float[] pad(final float[] src, final int srcElementSize, final int attribSize) {
+        if (srcElementSize == attribSize) {
+            return src;
+        }
+        final int elements = src.length / srcElementSize;
+        final float[] rVal = new float[elements * attribSize];
+
+        for (int i = 0; i < elements; i++) {
+            // index into src for our element data
+            final int srcStart = i * srcElementSize;
+            // index into rVal to store
+            final int dstStart = i * attribSize;
+            // Copy the data from src to rVal
+            System.arraycopy(src, srcStart, rVal, dstStart, srcElementSize);
+        }
+
+        return rVal;
+    }
+
     // /////////////////
     // Methods for Savable
     // /////////////////
@@ -531,73 +640,5 @@ public class SkinnedMesh extends Mesh implements PoseListener {
         if (_currentPose != null) {
             _currentPose.addPoseListener(this);
         }
-    }
-
-    private void createJointBuffer() {
-        final float[] data = reorderAndPad(convert(_jointIndices), getWeightsPerVert(), getGpuAttributeSize());
-        _jointIndicesBuf = BufferUtils.createFloatBuffer(_jointIndicesBuf, data);
-    }
-
-    private void createWeightBuffer() {
-        final float[] data = reorderAndPad(_weights, getWeightsPerVert(), getGpuAttributeSize());
-        _weightsBuf = BufferUtils.createFloatBuffer(_weightsBuf, data);
-    }
-
-    /**
-     * Convert a short array to a float array
-     * 
-     * @param shorts
-     *            the short values
-     * @return our new float array
-     */
-    private float[] convert(final short... shorts) {
-        final float[] rval = new float[shorts.length];
-        for (int i = 0; i < rval.length; i++) {
-            rval[i] = shorts[i];
-        }
-        return rval;
-    }
-
-    /**
-     * Rearrange the data from data per element, to a list of matSide x matSide matrices, output by row as such:
-     * 
-     * row0element0, row0element1, row0element2...<br>
-     * row1element0, row1element1, row1element2...<br>
-     * row2element0, row2element1, row2element2...<br>
-     * 
-     * If there is not enough values in the source data to fill out a row, 0 is used.
-     * 
-     * @param src
-     *            our source data, stored as element0, element1, etc.
-     * @param size
-     *            the number of values per element in our source element
-     * @param matSide
-     *            the size of the matrix edge... eg. 4 would mean a 4x4 matrix.
-     * @return our new data array.
-     */
-    private float[] reorderAndPad(final float[] src, final int size, final int matSide) {
-        final int elements = src.length / size;
-        final float[] rVal = new float[elements * matSide * matSide];
-
-        // size of each attribute (a row from each element)
-        final int length = matSide * elements;
-
-        for (int i = 0; i < elements; i++) {
-            // index into src for our element data
-            final int srcStart = i * size;
-            // index into a destination row.
-            final int dstOffset = i * matSide;
-
-            // Go through each row of the current src element. Go through only as many rows of data as we have.
-            // (eg. if size is 6 and matSide is 4, we only need to go through j=0 and j=1)
-            for (int j = 0; j <= (size - 1) / matSide; j++) {
-                // How much to copy. Generally matSide, except for last bit of data.
-                final int copySize = Math.min(size - j * matSide, matSide);
-                // Copy the data from src to rVal
-                System.arraycopy(src, srcStart + j * matSide, rVal, j * length + dstOffset, copySize);
-            }
-        }
-
-        return rVal;
     }
 }
