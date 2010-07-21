13f8c45425c93dd1044473ac58387b5bcd72bb9f
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2010-07-21 03:23:21 +0000

    Added some new useful trigger conditions
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@1309 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-core/src/main/java/com/ardor3d/input/logical/TriggerConditions.java
+++ ardor3d-core/src/main/java/com/ardor3d/input/logical/TriggerConditions.java
@@ -14,6 +14,8 @@ import java.util.EnumMap;
 
 import com.ardor3d.input.ButtonState;
 import com.ardor3d.input.MouseButton;
+import com.ardor3d.util.Timer;
+import com.google.common.base.Predicate;
 
 /**
  * Utility methods for getting standard TriggerConditions. To reduce object creation, it may be a good idea to use
@@ -25,6 +27,20 @@ public final class TriggerConditions {
     private static final MouseButtonCondition RIGHT_DOWN_CONDITION = makeCondition(MouseButton.RIGHT, ButtonState.DOWN);
     private static final MouseButtonCondition MIDDLE_DOWN_CONDITION = makeCondition(MouseButton.MIDDLE,
             ButtonState.DOWN);
+
+    private static final Predicate<TwoInputStates> ALWAYS_TRUE = new Predicate<TwoInputStates>() {
+        @Override
+        public boolean apply(final TwoInputStates arg0) {
+            return true;
+        }
+    };
+
+    private static final Predicate<TwoInputStates> ALWAYS_FALSE = new Predicate<TwoInputStates>() {
+        @Override
+        public boolean apply(final TwoInputStates arg0) {
+            return true;
+        }
+    };
 
     private static MouseButtonCondition makeCondition(final MouseButton button, final ButtonState state) {
         final EnumMap<MouseButton, ButtonState> map = new EnumMap<MouseButton, ButtonState>(MouseButton.class);
@@ -68,5 +84,38 @@ public final class TriggerConditions {
      */
     public static MouseButtonCondition middleButtonDown() {
         return MIDDLE_DOWN_CONDITION;
+    }
+
+    /**
+     * @return a condition that is always true.
+     */
+    public static Predicate<TwoInputStates> alwaysTrue() {
+        return ALWAYS_TRUE;
+    }
+
+    /**
+     * @return a condition that is always false.
+     */
+    public static Predicate<TwoInputStates> alwaysFalse() {
+        return ALWAYS_FALSE;
+    }
+
+    /**
+     * @return a condition that is always false.
+     */
+    public static Predicate<TwoInputStates> passedThrottle(final double throttleTime, final Timer timer) {
+        return new Predicate<TwoInputStates>() {
+            private double lastPass = 0;
+
+            @Override
+            public boolean apply(final TwoInputStates arg0) {
+                final double now = timer.getTimeInSeconds();
+                if (now - lastPass >= throttleTime) {
+                    lastPass = now;
+                    return true;
+                }
+                return false;
+            }
+        };
     }
 }
