cfb129865d84d7e604ad5c85d0ce6b86760f8eb3
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2010-09-06 00:00:34 +0000

    Added random access version of LittleEndian Data Input - originally written to support the L4D loader written by myself and David Bitkowski
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@1394 82abaa13-a93f-4eff-8987-b3ae9fc18050

---
+++ ardor3d-core/src/main/java/com/ardor3d/util/LittleEndianRandomAccessDataInput.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2008-2010 Ardor Labs, Inc.
+ *
+ * This file is part of Ardor3D.
+ *
+ * Ardor3D is free software: you can redistribute it and/or modify it 
+ * under the terms of its license which may be found in the accompanying
+ * LICENSE file or at <http://www.ardor3d.com/LICENSE>.
+ */
+
+package com.ardor3d.util;
+
+import java.io.DataInput;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import com.ardor3d.util.export.ByteUtils;
+
+/**
+ * Utility class useful for reading little-endian stored data in a random access fashion. All functions work as defined
+ * in DataInput, but assume they come from a LittleEndian input stream.
+ * 
+ * <p>
+ * Note: random access is implemented by reading the entire stream into memory.
+ * </p>
+ */
+public class LittleEndianRandomAccessDataInput implements DataInput {
+    private final ByteBuffer _contents;
+
+    /**
+     * Number of bytes to read when reading a char... For data meant to be read from C/C++ this is often 1, for Java and
+     * C# this is usually 2.
+     */
+    public int CHAR_SIZE = 2;
+
+    /**
+     * Creates a new LittleEndian reader from the given input stream. Note that this stream is loaded completely into
+     * memory.
+     * 
+     * @param in
+     *            The stream to read from.
+     */
+    public LittleEndianRandomAccessDataInput(final InputStream in) throws IOException {
+        _contents = ByteBuffer.wrap(ByteUtils.getByteContent(in));
+    }
+
+    /**
+     * Creates a new LittleEndian reader from the given byte buffer. Note that this byte buffer is not cloned or copied,
+     * so take care not to alter it during read. This constructor is useful for working with memory-mapped files.
+     * 
+     * @param contents
+     *            The contents to read from.
+     */
+    public LittleEndianRandomAccessDataInput(final ByteBuffer contents) throws IOException {
+        _contents = contents;
+    }
+
+    public final int readUnsignedShort() throws IOException {
+        return (readByte() & 0xff) | ((readByte() & 0xff) << 8);
+    }
+
+    public final long readUnsignedInt() throws IOException {
+        return ((readByte() & 0xff) | ((readByte() & 0xff) << 8) | ((readByte() & 0xff) << 16) | (((long) (readByte() & 0xff)) << 24));
+    }
+
+    public final boolean readBoolean() throws IOException {
+        return (readByte() != 0);
+    }
+
+    public final byte readByte() throws IOException {
+        return _contents.get();
+    }
+
+    public final int readUnsignedByte() throws IOException {
+        return readByte() & 0xff;
+    }
+
+    public final short readShort() throws IOException {
+        return (short) readUnsignedShort();
+    }
+
+    public final char readChar() throws IOException {
+        return (char) readUnsignedShort();
+    }
+
+    public final int readInt() throws IOException {
+        return ((readByte() & 0xff) | ((readByte() & 0xff) << 8) | ((readByte() & 0xff) << 16) | ((readByte() & 0xff) << 24));
+    }
+
+    public final long readLong() throws IOException {
+        return ((readByte() & 0xff) | ((long) (readByte() & 0xff) << 8) | ((long) (readByte() & 0xff) << 16)
+                | ((long) (readByte() & 0xff) << 24) | ((long) (readByte() & 0xff) << 32)
+                | ((long) (readByte() & 0xff) << 40) | ((long) (readByte() & 0xff) << 48) | ((long) (readByte() & 0xff) << 56));
+    }
+
+    public final float readFloat() throws IOException {
+        return Float.intBitsToFloat(readInt());
+    }
+
+    public final double readDouble() throws IOException {
+        return Double.longBitsToDouble(readLong());
+    }
+
+    public final void readFully(final byte b[]) throws IOException {
+        readFully(b, 0, b.length);
+    }
+
+    public final void readFully(final byte b[], final int off, final int len) throws IOException {
+        if (len - off + _contents.position() > _contents.capacity()) {
+            throw new EOFException("EOF reached");
+        } else {
+            _contents.get(b, off, len);
+        }
+    }
+
+    public final int skipBytes(final int n) throws IOException {
+        if (_contents.remaining() >= n) {
+            _contents.position(_contents.position() + n);
+            return n;
+        }
+        final int skipped = _contents.remaining();
+        _contents.position(_contents.limit());
+        return skipped;
+    }
+
+    /**
+     * Unsupported.
+     * 
+     * @throws IOException
+     *             if this method is called.
+     */
+    public final String readLine() throws IOException {
+        throw new IOException("operation unsupported.");
+    }
+
+    /**
+     * Unsupported.
+     * 
+     * @throws IOException
+     *             if this method is called.
+     */
+    public final String readUTF() throws IOException {
+        throw new IOException("operation unsupported.");
+    }
+
+    public final void seek(final int pos) throws IOException {
+        _contents.position(pos);
+    }
+
+    public int position() {
+        return _contents.position();
+    }
+
+    public int capacity() {
+        return _contents.capacity();
+    }
+}
