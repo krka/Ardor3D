157a32c8ce25f1e11c5e0089b3e91f25da3cb3a2
ardorlabs <ardorlabs@82abaa13-a93f-4eff-8987-b3ae9fc18050>
2010-12-13 21:56:29 +0000

    Added a cleanup call
    git-svn-id: http://ardorlabs.svn.cvsdude.com/Ardor3Dv1/trunk@1499 82abaa13-a93f-4eff-8987-b3ae9fc18050

--- ardor3d-examples/src/main/java/com/ardor3d/example/canvas/ExampleScene.java
+++ ardor3d-examples/src/main/java/com/ardor3d/example/canvas/ExampleScene.java
@@ -17,6 +17,7 @@ import com.ardor3d.math.Ray3;
 import com.ardor3d.renderer.ContextManager;
 import com.ardor3d.renderer.Renderer;
 import com.ardor3d.scenegraph.Node;
+import com.ardor3d.util.ContextGarbageCollector;
 import com.ardor3d.util.GameTaskQueue;
 import com.ardor3d.util.GameTaskQueueManager;
 
@@ -40,6 +41,7 @@ public final class ExampleScene implements Scene {
         // Execute renderQueue item
         GameTaskQueueManager.getManager(ContextManager.getCurrentContext()).getQueue(GameTaskQueue.RENDER).execute(
                 renderer);
+        ContextGarbageCollector.doRuntimeCleanup(renderer);
 
         renderer.draw(root);
         return true;
